b418395678c33db8da9649f0cb2628d899398dbc
Danf96 <danielthomasf@gmail.com>
2022-11-15 16:09:50 -0800

    Remove dir, initial indexer, separate classes.

--- server_whoosh.py
+++ server_whoosh.py
@@ -1,12 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
-import whoosh
-from whoosh.index import create_in
-from whoosh.index import open_dir
-from whoosh.fields import *
-from whoosh.qparser import QueryParser
-from whoosh.qparser import MultifieldParser
-from whoosh import qparser
 from operator import itemgetter
+from mywhoosh import *
 
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
@@ -58,36 +52,7 @@ def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('404.html'), 404
 
-class MyWhooshSearcher(object):
-	"""docstring for MyWhooshSearcher"""
-	def __init__(self):
-		super(MyWhooshSearcher, self).__init__()
-		
-		
-	def search(self, queryEntered):
-		title = list()
-		description = list()
-		with self.indexer.searcher() as search:
-			query = MultifieldParser(['title', 'description'], schema=self.indexer.schema)
-			query = query.parse(queryEntered)
-			results = search.search(query, limit=None)
-			
-			for x in results:
-				title.append(x['title'])
-				description.append(x['description'])
-			
-		return title, description
 
-	def index(self):
-		schema = Schema(id=ID(stored=True), title=TEXT(stored=True), description=TEXT(stored=True))
-		indexer = create_in('myIndex', schema)
-		writer = indexer.writer()
-
-		writer.add_document(id=u'1', title=u'hello there', description=u'cs hello, how are you')
-		writer.add_document(id=u'2', title=u'hello bye', description=u'nice to meetcha')
-		writer.commit()
-
-		self.indexer = indexer
 
 # indexer = index()
 # search(indexer, 'nice')
@@ -95,7 +60,7 @@ class MyWhooshSearcher(object):
 if __name__ == '__main__':
 	# global mySearcher
 	# mySearcher = MyWhooshSearcher()
-	# mySearcher.index()
+	# mySearcher.build_index()
 	#title, description = mySearcher.search('hello')
 	#print(title)
 	app.run(debug=True)
