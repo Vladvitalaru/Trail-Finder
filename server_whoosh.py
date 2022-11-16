59972e66a38803a70eae40ae0a98f310d0e8332f
VladVitalaru <vladvitalaru@yahoo.com>
2022-11-16 15:54:51 -0800

    Refactor advanced search & routing

--- server_whoosh.py
+++ server_whoosh.py
@@ -8,34 +8,18 @@ app = Flask(__name__)
 def index():
 	return render_template('Home.html')
 
+
 @app.route('/results/', methods=['GET', 'POST'])
 def results():
 	# # global mySearcher
-	# data = request
 	if request.method == 'POST':
-		# query = request.form.get("search")
-		# query = search.get('search')
-		state = request.form.get("state")
 		query = request.form.get("search")
-		# state = request.form['state']
-		# state = advanced.get('state')
-		# county = data.get('county')
-		# print(query)
-		# state = request.form['state']
+		print(query)
 
-		print(state, query)
 
-		# print(state)
-		# state = "test"
-		# print(county)
-	# 	return redirect(url_for('results', input=data))
-	# else:
-	# 	# input = request.form["search"] 
-	# results = [{'title': 'Example Trail Title', 'length': '2.1 Miles'},
-    #        {'title': 'Example Trail Title2', 'length': '3.1 Miles'}]
 	links = ["https://www.traillink.com/trail/peninsula-crossing-trail/","https://www.traillink.com/trail/westside-trail/","https://www.traillink.com/trail/vera-katz-eastbank-esplanade/"]
 	titles = ["Peninsula Crossing Trail","Westside Trail", "Vera Katz Eastbank Esplanade"]
-	lengths = ['2.1 Miles', '3.1 Miles', '10.1 Miles']
+	lengths = ['2.1', '3.1', '10.1']
 	images = ["https://cloudfront.traillink.com/photos/peninsula-crossing-trail_23816_sc.jpg",
            "https://cloudfront.traillink.com/photos/westside-trail_107049_sc.jpg",
            "https://cloudfront.traillink.com/photos/vera-katz-eastbank-esplanade_167510_sc.jpg"]
@@ -44,27 +28,40 @@ def results():
 	descriptions = ["Peninsula Crossing Trail spans 5.1 from N. Carey Blvd. and N. Princeton St. to Columbia Slough Trail at N. Columbia Blvd.","Westside Trail spans 8.1 from Forest Park to Barrows Park.","Vera Katz Eastbank Esplanade spans 1.7 from Steel Bridge just west of NE Lloyd Blvd. to SE Caruthers St. just south of the Marquam Bridge."]
 	styleID = [1,2,3]
 	return render_template('results.html', results = zip(links, titles, lengths, images, states, county, descriptions, styleID) )
- 
- 
-	# 	data = request.args
 
-	# query = data.get('searchterm')
-	# test = data.get('test')
-	# titles, description = mySearcher.search(query)
-	# print("You searched for: " + query)
-	# print("Alternatively, the second box has: " + test)
- 
-	# return render_template('results.html', query=query, results=zip(titles, description))
-	#return "Hello world!"
-	# return render_template('results.html')
 
+@app.route('/advancedResults/', methods=['GET', 'POST'])
+def advancedResults():
+	# # global mySearcher
+	if request.method == 'POST':
+		state = request.form.get("state")
+		county = request.form.get("county")
+		minlength = request.form.get("minLength")
+		maxlength = request.form.get("maxLength")
+		activities = request.form.get("activities")
+		surfaces = request.form.get("surfaces")
+		advancedSearch = request.form.get("advancedSearch")
+
+		print("advancedResults")
+		print(state, county, minlength, maxlength, activities, surfaces, advancedSearch )
+
+	links = ["https://www.traillink.com/trail/peninsula-crossing-trail/","https://www.traillink.com/trail/westside-trail/","https://www.traillink.com/trail/vera-katz-eastbank-esplanade/"]
+	titles = ["Peninsula Crossing Trail","Westside Trail", "Vera Katz Eastbank Esplanade"]
+	lengths = ['2.1', '3.1', '10.1']
+	images = ["https://cloudfront.traillink.com/photos/peninsula-crossing-trail_23816_sc.jpg",
+           "https://cloudfront.traillink.com/photos/westside-trail_107049_sc.jpg",
+           "https://cloudfront.traillink.com/photos/vera-katz-eastbank-esplanade_167510_sc.jpg"]
+	states = ["Oregon", "Oregon", "Oregon"]
+	county = ["Multnomah", "Washington","Multnomah" ]
+	descriptions = ["Peninsula Crossing Trail spans 5.1 from N. Carey Blvd. and N. Princeton St. to Columbia Slough Trail at N. Columbia Blvd.","Westside Trail spans 8.1 from Forest Park to Barrows Park.","Vera Katz Eastbank Esplanade spans 1.7 from Steel Bridge just west of NE Lloyd Blvd. to SE Caruthers St. just south of the Marquam Bridge."]
+	styleID = [1,2,3]
+	return render_template('advancedResults.html', results = zip(links, titles, lengths, images, states, county, descriptions, styleID) )
+ 
 #Handle error 404
 @app.errorhandler(404)
 def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('404.html'), 404
-
-
 
 # indexer = index()
 # search(indexer, 'nice')
