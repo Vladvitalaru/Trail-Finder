244f60cc16e43bf05d1d334ef9c71fd9f081168e
Danf96 <danielthomasf@gmail.com>
2022-11-16 17:10:33 -0800

    Re-split whoosh code with flask code.

--- mywhoosh.py
+++ mywhoosh.py
@@ -9,31 +9,34 @@ from whoosh import qparser
 import os
 import re
 import json
+import random
 
 
 class MyWhooshSearcher(object):
 	"""Class to be used for indexing and searching"""
 	def __init__(self):
 		super(MyWhooshSearcher, self).__init__()
-		
-		
+
 	def search(self, queryEntered):
 		"""Yet to be updated, should be updated to a general search with all the necessary fields and an advanced search as two separate methods"""
-		title = list()
-		state = list()
-		county = list()
+		url, title, length, image, state, county, description, styleID = ([] for _ in range(8))
+		for _ in range(0, 10): styleID.append(random.randint(1, 4)) # generate random styleID list for testing
 		with self.indexer.searcher() as search:
-			query = MultifieldParser(['title', 'state', 'county', 'activities'], schema=self.indexer.schema)
+			query = MultifieldParser(['url', 'length', 'title', 'image', 'state', 'county', 'description', 'activities'], schema=self.indexer.schema)
 			query = query.parse(queryEntered)
 			results = search.search(query, limit=10)
-			
 			for x in results:
-				title.append(x['title'])
-				state.append(x['state'])
-				county.append(x['county'])
-			
-		return title, state, county
-	
+				if x['url'].endswith('trail-detail-reviews'): pass #will want to remove once crawler is fixed
+				else:
+					url.append(x['url'])
+					title.append(x['title'])
+					length.append(str(x['length'])+' Miles')
+					image.append(x['image'])
+					state.append(x['state'])
+					county.append(x['county'])
+					description.append(x['description'])
+		return url, title, length, image, state, county, description, styleID
+
 	def existing_index(self):
 		"""Loads an existing index at /myIndex/"""
 		self.indexer = open_dir('myIndex')
@@ -42,37 +45,27 @@ class MyWhooshSearcher(object):
 		"""Creates a new index based on the file path given containing documents"""
 		corpus_path = "./files"
 		schema = Schema(url=ID(stored=True), title=TEXT(stored=True), length=NUMERIC(int, decimal_places=2,stored=True, signed=False), 
-			image=STORED, state=KEYWORD(stored=True,commas=True), county=KEYWORD(stored=True,commas=True), 
-				description=STORED, trail_surfaces=KEYWORD(commas=True), activities=KEYWORD(commas=True), content=KEYWORD)
+			image=TEXT(stored=True), state=KEYWORD(stored=True,commas=True), county=KEYWORD(stored=True,commas=True), 
+			description=TEXT(stored=True), trail_surfaces=KEYWORD(commas=True), activities=KEYWORD(commas=True), content=KEYWORD)
 		indexer = create_in('myIndex', schema)
 		writer = indexer.writer()
 		file_names = os.listdir(corpus_path)
-		#i = 0
 		for name in file_names:
 			full_path = os.path.join(corpus_path, name)
 			if os.path.isfile(full_path): #make sure we aren't reading directories
-				#open file and begin parsing
-				#i += 1
-				with open(full_path, 'r') as f:
+				with open(full_path, 'r') as f: #open file and begin parsing
 					json_dict = json.loads(f.readline())
-					if len(json_dict['images']) > 0:
-						image_url = json_dict['images'][0]
-					else:
-						image_url = "https://www.traillink.com/images/tl/placeholders/No_Photo_Image_Thumbnail-trimmed.jpg"
+					if len(json_dict['images']) > 0: image_url = json_dict['images'][0]
+					else: image_url = "https://www.traillink.com/images/tl/placeholders/No_Photo_Image_Thumbnail-trimmed.jpg"
 					activity_string = ",".join(json_dict['facts']['activities'])
 					review_cloud_path = "cloud.png" #temporary path based on our current default word cloud image
 					len_dec = Decimal(json_dict['facts']['Length'].rstrip(" miles"))
 					writer.add_document(url=json_dict['url'], title=json_dict['title'], length=len_dec,
-					image=image_url, state=json_dict['facts']['States'], county=json_dict['facts']['Counties'],
-					description=json_dict['description'], trail_surfaces=json_dict['facts']['Trail surfaces'],
-					activities=activity_string, content=json_dict['content'])
-
+						image=image_url, state=json_dict['facts']['States'], county=json_dict['facts']['Counties'],
+						description=json_dict['description'], trail_surfaces=json_dict['facts']['Trail surfaces'],
+						activities=activity_string, content=json_dict['content'])
 		writer.commit()
-
 		self.indexer = indexer
-
-# indexer = index()
-# search(indexer, 'nice')
 
 if __name__ == '__main__':
 	# global mySearcher #may want to uncomment?
