72c17a43614f9b728b0bc5e862fcb8e1209cfcff
VladVitalaru <vladvitalaru@yahoo.com>
2022-11-17 18:41:48 -0800

    Live suggestions

--- static/Scripts.js
+++ static/Scripts.js
@@ -24,3 +24,85 @@ function openFilters() {
     }
 
 }
+
+let titles = [
+    "Greenway",
+    "Hamilton",
+    "Rock",
+    "Pier",
+    "Hertage",
+    "Herring",
+    "Herrold",
+    "Harbor",
+    "Heel",
+]   
+
+const seachWrapper = document.querySelector(".inputs");
+const inputBox = document.querySelector("input");
+const suggestionsBox = document.querySelector(".suggestionsBox");
+
+// User pressing keys
+inputBox.onkeyup = (e)=> {
+    let userData = e.target.value; // user input data
+    let emptyArray = []
+    if (userData) {
+        emptyArray = titles.filter((data)=>{
+            // Filter array values to lowercase and return titles which start with user entered data
+            return data.toLocaleUpperCase().startsWith(userData.toLocaleUpperCase());
+        });
+        emptyArray = emptyArray.map((data)=>{
+            return data = '<li>' + data + '</li>';
+        });
+        console.log(emptyArray);
+        // suggestionsBox.classList.add("active");
+
+        showSuggestions(emptyArray);
+
+        let allList = suggestionsBox.querySelectorAll("li");
+        console.log(allList)
+        for (let i = 0; i < allList.length; i++) {
+            // on click ability for suggestions
+            allList[i].setAttribute("onclick", "select(this)");
+        }
+    } else {
+
+    }
+}
+
+function select(element) {
+    let selectUserData = element.textContent
+    console.log(selectUserData)
+    inputBox.value = selectUserData; // set input as clicked suggestion
+}
+
+function showSuggestions(list) {
+    let listData;
+
+    // Show own input if no suggestions
+    if(!list.length) {
+        userInput = inputBox.value;
+        listData = `<li>${userInput}</li>`;
+
+    // if more than 5 suggestions, only show 5
+    }else if (list.length >= 5) {
+
+        let max = 5
+        let spliced = []
+
+        for (var i = 0; i < max; i++) {
+            spliced.push(list[i]);
+        }
+       listData = spliced.join('');
+    
+    // Show suggestions
+    } else {
+        listData = list.join('');
+    }
+
+    console.log(listData)
+    if (typeof listData === 'undefined') {
+       listData = '<li> </li>';
+    };
+ 
+    suggestionsBox.innerHTML = listData;
+}
