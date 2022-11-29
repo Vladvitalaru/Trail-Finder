8ddd4e133ea94deae21abf49b1ac92b6cd15e3f2
Johnathan Chan <70113422+chanjc14@users.noreply.github.com>
2022-11-28 21:02:32 -0800

    Incorporate pagerank, compiled PR into single file

--- pagerank_calculator.py
+++ pagerank_calculator.py
@@ -2,17 +2,22 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from networkx.drawing.nx_agraph import graphviz_layout
 from matplotlib.backends.backend_pdf import PdfPages
+import json
 """Will be used to calculate pagerank based off of adjacent links file generated from crawler, will later need to store 'pr' as a dictionary to refer to when indexing in mywhoosh.py
 as well as removing the plotting and saving figures, only used for debugging right now"""
 def main():
-    DG = nx.read_gpickle("./adjacent_links.gpickle")
-    f = plt.figure(figsize=(100,100))
-    plt.subplot(121)
+    DG = nx.read_gpickle("./adjacent_links_traillink.gpickle")
+    DG2 = nx.read_gpickle("./adjacent_links_oregonhikers.gpickle")
+    #f = plt.figure(figsize=(100,100))
+    #plt.subplot(121)
+    DG = nx.compose(DG, DG2)
     pr = nx.pagerank_numpy(DG, weight=None)
-    print(pr)
-    nx.draw(DG, with_labels=True, font_weight='bold')
-    plt.show()
-    f.savefig("Example1.pdf", bbox_inches='tight')
+    with open('traillinkPR.txt', 'w') as f:
+        f.write(json.dumps(pr))
+    #print(pr)
+    #nx.draw(DG, with_labels=True, font_weight='bold')
+    #plt.show()
+    #f.savefig("Example1.pdf", bbox_inches='tight')
 
 if __name__ == '__main__':
 	main()
